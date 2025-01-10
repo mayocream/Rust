4bde33dff97bb576c6fc3587bd5431365aee8bca
Mayo <mayo@linux.com>
2025-01-10 23:32:23 +0900

    Add enums module with IpAddrKind enum and example usage

---
+++ enums/src/main.rs
@@ -0,0 +1,20 @@
+#[allow(unused)]
+
+#[derive(Debug)]
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    println!("home: {:?}", home);
+    println!("loopback: {:?}", loopback);
+
+    // Option<T> enum
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+}
