7d50bac062cad91dcff9b84576368dae40023389
Mayo <mayo@linux.com>
2025-01-10 22:51:12 +0900

    Enhance Rectangle struct with area calculation, width check, containment check, and square creation method

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,29 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
@@ -15,12 +38,28 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
     println!("rect1 is {:#?}", rect1);
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    if rect1.width() {
+        println!("rect1 has a width");
+    }
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(3);
+    println!("square is {:#?}", square);
 }
