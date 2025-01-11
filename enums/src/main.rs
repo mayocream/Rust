71512be33ef8d47e3bb9356a05337f89ef2f914d
Mayo <mayo@linux.com>
2025-01-11 16:47:43 +0900

    Add back_of_house module with Breakfast struct and summer method

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,4 +1,4 @@
-#[allow(unused)]
+#![allow(unused)]
 
 #[derive(Debug)]
 enum IpAddrKind {
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -13,10 +13,32 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
