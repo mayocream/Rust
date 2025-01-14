935b7639b78c89eb57ea617fbe95b2458466c33d
Mayo <mayo@linux.com>
2025-01-15 00:49:56 +0900

    Implement default summarize method for Summary trait and update NewsArticle struct

--- generics/src/traits.rs
+++ generics/src/traits.rs
@@ -1,8 +1,12 @@
 #![allow(unused)]
 
 mod aggregator {
+    use std::fmt::Display;
+
     pub trait Summary {
-        fn summarize(&self) -> String;
+        fn summarize(&self) -> String {
+            String::from("(Read more...)")
+        }
     }
 
     pub struct NewsArticle {
@@ -12,11 +16,7 @@ mod aggregator {
         pub content: String,
     }
 
-    impl Summary for NewsArticle {
-        fn summarize(&self) -> String {
-            format!("{}, by {} ({})", self.headline, self.author, self.location)
-        }
-    }
+    impl Summary for NewsArticle {}
 
     pub struct Tweet {
         pub username: String,
@@ -31,9 +31,12 @@ mod aggregator {
         }
     }
 
+    pub fn notify(item: &(impl Summary + Display)) {
+        println!("Breaking news! {}", item.summarize());
+    }
 }
 
-use aggregator::{Summary, Tweet};
+use aggregator::{Summary, Tweet, NewsArticle};
 
 fn main() {
     let tweet = Tweet {
@@ -44,4 +47,13 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
