98059f21057334b38754132c05ff6b081ea09f26
Mayo <mayo@linux.com>
2025-01-15 21:37:33 +0900

    Add lifetimes module with ImportantExcerpt struct and longest function

---
+++ generics/src/lifetimes.rs
@@ -0,0 +1,58 @@
+#![allow(unused)]
+
+// ImportantExcerpt struct has a lifetime parameter 'a
+// which is shorter or equal to the lifetime of the str
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn main() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    // lifetime annotations in struct definitions
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    // static lifetime
+    let s: &'static str = "I have a static lifetime.";
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: std::fmt::Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
